18aed8aeb0165e890f0f4b2c65032b914dbb0e18
FanHuaRan <834171100@qq.com>
2017-11-28 18:31:32 +0800

    11/28

---
+++ simple-quickstart/src/main/java/com/evan/simple_quickstart/App.java
@@ -0,0 +1,13 @@
+package com.evan.simple_quickstart;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
